3ea040641eab91ca3394bf599802135fe16c644f
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-06-21 00:19:11 +0100

    implement login / register functionality

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,39 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import ReturnToHome from '../components/partials/returnToHome'
+import { useAuth } from '../context/AuthContext'
 
 const Login: NextPage = () => {
+  const { user, loginWithDetails } = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (user) {
+      router.push("/dashboard")
+    }
+  }, [user])
+
+  const submitUserForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const form = e.currentTarget
+    const formData = new FormData(form)
+
+    let email = formData.get("email") as string
+    let password = formData.get("password") as string
+
+    const loggedInUser = await loginWithDetails(email, password);
+  }
+
   return (
-    <div className='bg-main'>
+    <div className='bg-main h-screen'>
       <Head><title>Pong | Register</title></Head>
-      <main className='flex justify-center p-8'>
+      <main className='flex justify-center items-center p-8 min-h-96 h-4/5'>
         <section className='flex flex-col w-full max-w-xs mx-auto'>
           <h1 className='text-4xl font-medium text-center mb-4'>Let's Go! 🏓</h1>
-          <form action="/api/login">
+          <form onSubmit={submitUserForm}>
             <div className='flex flex-col md:justify-center space-y-4 mt-4'>
               <input type="email" name="email" placeholder="Email" required className='text-gray-700 w-full p-3 rounded-md border-2 border-gray-200' />
               <input type="password" name="password" placeholder="Password" required className='text-gray-700 w-full p-3 rounded-md border-2 border-gray-200' />
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,26 +1,60 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import ReturnToHome from '../components/partials/returnToHome'
+import { useAuth } from '../context/AuthContext'
 
 const Register: NextPage = () => {
+  const { user, createUserWithDetails } = useAuth()
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push('/dashboard')
+    }
+  }, [user])
+
+  const submitUserForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const form = e.currentTarget
+    const formData = new FormData(form)
+
+    if (formData.get('password') !== formData.get('confirmPassword')) {
+      return alert('Passwords do not match');
+    }
+
+    const newUser = {
+      name: formData.get('name') as string,
+      email: formData.get('email') as string,
+      password: formData.get('password') as string
+    }
+
+    const createdUser = await createUserWithDetails(newUser);
+    console.log(createdUser);
+    if (createdUser) {
+      router.push('/dashboard');
+    }
+  };
+
   return (
-    <div className='bg-main'>
+    <div className='bg-main h-screen'>
       <Head><title>Pong | Register</title></Head>
-      <main className='flex justify-center p-8'>
+      <main className='flex justify-center items-center p-8 h-4/5'>
         <section className='flex flex-col w-full max-w-xs mx-auto'>
           <h1 className='text-4xl font-medium text-center mb-4'>Start Playing 🏓</h1>
-          <form action="/api/register">
+          <form onSubmit={submitUserForm}>
             <div className='flex flex-col md:justify-center space-y-4 mt-4'>
-                <input type="name" name="name" placeholder="Name" required className='text-gray-700 w-full p-3 rounded-md border-2 border-gray-200' />
-                <input type="email" name="email" placeholder="Email" required className='text-gray-700 w-full p-3 rounded-md border-2 border-gray-200' />
-                <input type="password" name="password" placeholder="Password" required className='text-gray-700 w-full p-3 rounded-md border-2 border-gray-200' />
-                <input type="password" name="confirmPassword" placeholder="Confirm Password" required className='text-gray-700 w-full p-3 rounded-md border-2 border-gray-200' />
+              <input type="name" name="name" placeholder="Name" required className='text-gray-700 w-full p-3 rounded-md border-2 border-gray-200' />
+              <input type="email" name="email" placeholder="Email" required className='text-gray-700 w-full p-3 rounded-md border-2 border-gray-200' />
+              <input type="password" name="password" placeholder="Password" required className='text-gray-700 w-full p-3 rounded-md border-2 border-gray-200' />
+              <input type="password" name="confirmPassword" placeholder="Confirm Password" required className='text-gray-700 w-full p-3 rounded-md border-2 border-gray-200' />
             </div>
             <div className='flex flex-col justify-center'>
-                <input type="submit" value='Register' className='my-4 text-center py-4 rounded-sm bg-blue-800 hover:bg-blue-900 w-full mx-auto' />
-                <p className='text-center'>Already have an account?<Link href="/login"><a className='ml-2 text-blue-500 hover:text-blue-700'>Login</a></Link></p>
-                <ReturnToHome />
+              <input type="submit" value='Register' className='my-4 text-center py-4 rounded-sm bg-blue-800 hover:bg-blue-900 w-full mx-auto' />
+              <p className='text-center'>Already have an account?<Link href="/login"><a className='ml-2 text-blue-500 hover:text-blue-700'>Login</a></Link></p>
+              <ReturnToHome />
             </div>
           </form>
         </section>
