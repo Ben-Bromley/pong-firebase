b36c489aada9cfb9a55d295672724bfb0da4c987
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-06-21 18:53:17 +0100

    improve GET user endpoint, add uid option

--- pages/api/users/get.ts
+++ pages/api/users/get.ts
@@ -1,22 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { db } from "../../../firebaseAdmin"; 
+import { db } from "../../../firebaseAdmin";
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
 
-    const email = req.query.email as string;
+    if (req.method === "GET") {
+        const email = req.query.email as string;
+        const uid = req.query.uid as string;
 
-    if (!email) {
-        res.status(400).json({status: 400, message: "Missing email"});
+        let response;
+
+        if (email) {
+            // get user by email. if empty, return 404, otherwise return user data
+            response = await db.collection('users').where('email', '==', email).get();
+            if (response.empty) {
+                res.status(404).json({ status: 404, message: "User not found" });
+            }
+            response = response.docs[0].data();
+        } else if (uid) {
+            // get user by uid, if empty, return 404, if not empty, return user data
+            response = await db.collection('users').doc(uid).get();
+            if (!response.exists) {
+                res.status(404).json({ status: 404, message: "User not found" });
+            }
+            response = response.data();
+        } else {
+            // return error if user identifier is not provided
+            res.status(400).json({ status: 400, message: "Missing user identifier (uid or email)" });
+        }
+
+        res.status(200).json({ status: 200, message: "success", data: response });
+
+    } else {
+        res.status(405).json({ status: 405, message: "Method not allowed" });
     }
-
-    // get user by email
-    const response = await db.collection('users').where('email', '==', email).get();
-
-    if (response.empty) {
-        res.status(404).json({status: 404, message: "User not found"});
-    }
-
-    res.status(200).json({status: 200, message: "success", data: response});
 }
