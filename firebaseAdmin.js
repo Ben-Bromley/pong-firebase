37d6c146bb47597a33b40fe804d89dc5b8c58ada
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-06-21 15:05:09 +0100

    fix firebase-admin issue

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,15 +1,11 @@
-import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 var serviceAccount = require("./secrets.json");
 
 if (getApps().length === 0) {
-	initializeApp(
-		{
-			credential: cert(serviceAccount),
-		},
-		"firebaseAdmin"
-	);
+	initializeApp({ credential: cert(serviceAccount) }, "firebaseAdmin");
 }
 
-export const db = getFirestore();
+// using getApp() to load the correct firebase instace
+export const db = getFirestore(getApp("firebaseAdmin"));
