d44a852b81e2650400c331a0781596fecd8ddb0c
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-06-25 16:28:43 +0100

    fix: creating user returned wrong UID

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,8 +1,7 @@
 import { createContext, useContext, useState } from "react";
-import { signOut, deleteUser } from "firebase/auth";
+import { signOut, deleteUser, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import {
 	useAuthState,
-	useCreateUserWithEmailAndPassword,
 	useSignInWithEmailAndPassword,
 } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
@@ -12,46 +11,49 @@ import { collection, query, where, getDoc, doc } from "firebase/firestore";
 const AuthContext = createContext({});
 
 const fetchUserData = async (uid) => {
-	let response = await fetch("/api/user/get?uid=" + uid);
-	data = await response.json();
+	console.log("Fetching: ", uid);
+	let response = await fetch("/api/users/get?uid=" + uid);
+	response = await response.json();
+	return response.data;
+};
+
+const initUserData = async (uid, addData, name) => {
+	console.log("Init data for: ", uid);
+	let response = await fetch("/api/users/create", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({
+			uid: uid,
+			email: addData.email,
+			name: name,
+			// organisation: addData.organisation
+		}),
+	});
+	let data = await response.json();
 	return data;
 };
 
 export const AuthContextProvider = ({ children }) => {
+	const [userData, setUserData] = useState(null);
+
 	const [user, loading, error] = useAuthState(auth, {
 		onUserChanged: async (user) => {
 			if (user) {
-				// get user data via API
+				// get user data and set in state
 				let data = await fetchUserData(user.uid);
-				// set user data in state
 				setUserData(data);
 			} else {
+				// clear user data
 				setUserData(null);
 			}
-			// get user doc where email is the same as the user's email
-			const userCollection = collection("users");
-			const userDoc = await getDoc(
-				userCollection,
-				where("email", "==", user.email)
-			);
 		},
 	});
 
-	const [userData, setUserData] = useState(null);
-	const [
-		createUserWithEmailAndPassword,
-		newUser,
-		newUserLoading,
-		newUserError,
-	] = useCreateUserWithEmailAndPassword(auth);
-	const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
-
 	const loginWithDetails = (email, password) => {
-		return signInWithEmailAndPassword(email, password);
+		return signInWithEmailAndPassword(auth, email, password);
 	};
-	const logout = () => {
-		signOut(auth);
-	};
+
+	const logout = () => signOut(auth);
 
 	const createUserWithDetails = (details) => {
 		const createNewFirebaseUser = async (details) => {
@@ -59,49 +61,23 @@ export const AuthContextProvider = ({ children }) => {
 			if (details.password !== details.passwordConfirm) {
 				return alert("Passwords do not match");
 			}
-
 			// create user in auth with firebase
-			await createUserWithEmailAndPassword(
-				details.email,
-				details.password
-			);
-			if (newUserError) return alert(newUserError.message);
-
-			if (newUser) {
-				// if new user is created, add to firebase
-				console.log("Adding Data to Firestore...");
-				// TODO: Check return data to try and get the correct UID
-				// console.log("NEW USER DATA: ", newUser);
+			createUserWithEmailAndPassword(auth, details.email, details.password)
+			.then(async (userCredential)=>{
+				const newUser = userCredential.user;
 				try {
-					let response = await fetch("/api/users/create", {
-						method: "POST",
-						headers: { "Content-Type": "application/json" },
-						body: JSON.stringify({
-							uid: newUser.user.uid,
-							email: newUser.user.email,
-							name: details?.name,
-							// organisation: organisation
-						}),
-					});
-					let data = await response.json();
-					// log response from API
-					console.log(data);
+					// if new user is created, add to firebase
+					await initUserData(newUser.uid, newUser, details?.name);
 				} catch (e) {
-					console.log(
-						"Failed to add data to firestore, removing account"
-					);
+					// remove user if data not added to firestore
 					deleteUser(auth.currentUser);
-					console.log("User deleted");
 				}
-				let currentUserData = await fetchUserData(newUser.user.uid);
+				let currentUserData = await fetchUserData(newUser.uid);
 				setUserData(currentUserData);
 				return { message: "success" };
-			} else {
-				console.log("Could not find new user, removing account");
-				deleteUser(auth.currentUser);
-				console.log("User deleted");
-				return { message: "fail" };
-			}
+			}).catch((error)=>{
+				return alert(error.message);
+			});
 		};
 
 		return createNewFirebaseUser(details);
