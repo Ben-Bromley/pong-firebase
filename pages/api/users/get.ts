105cac4197a47012c276ad1f88e046085121c285
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-07-08 20:47:01 +0100

    refactor file (rewritten but better)

--- pages/api/users/get.ts
+++ pages/api/users/get.ts
@@ -5,42 +5,49 @@ export default async function handler(
     res: NextApiResponse
 ) {
 
-    if (req.method === "GET") {
-        const email = req.query.email as string;
-        const uid = req.query.uid as string;
-        const team = req.query.team as string;
-
-        let response: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData> | FirebaseFirestore.DocumentSnapshot<FirebaseFirestore.DocumentData>
-        let returnData: FirebaseFirestore.DocumentData | undefined
-
-        if (team) {
-            response = await db.collection("users").where("team", "==", team).get();
-            if (response.empty) {
-                res.status(404).json({ status: 404, message: "No users found" })
-            }
-            returnData = response.docs[0].data()
-        } else if (email) {
-            // get user by email. if empty, return 404, otherwise return user data
-            response = await db.collection('users').where('email', '==', email).get();
-            if (response.empty) {
-                return res.status(404).json({ status: 404, message: "User not found" });
-            }
-            returnData = response.docs[0].data();
-        } else if (uid) {
-            // get user by uid, if empty, return 404, if not empty, return user data
-            response = await db.collection('users').doc(uid).get();
-            if (!response.exists) {
-                return res.status(404).json({ status: 404, message: "User not found" });
-            }
-            returnData = response.data();
-        } else {
-            // return error if user identifier is not provided
-            return res.status(400).json({ status: 400, message: "Missing user identifier (uid or email)" });
-        }
-
-        return res.status(200).json({ status: 200, message: "success", data: returnData });
-
-    } else {
+    if (req.method !== "GET") {
         return res.status(405).json({ status: 405, message: "Method not allowed" });
     }
+
+    const email = req.query.email as string;
+    const uid = req.query.uid as string;
+    const team = req.query.team as string;
+
+    let response: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData> | FirebaseFirestore.DocumentSnapshot<FirebaseFirestore.DocumentData> | undefined;
+    let returnData: FirebaseFirestore.DocumentData | null
+
+    // put uid first as it gets the doc slightly differently (TODO: add uid as attribute)
+    if (uid) {
+        // get user by uid, if empty, return 404, if not empty, return user data
+        response = await db.collection('users').doc(uid).get();
+        if (!response.exists) { 
+            return res.status(404).json({ status: 404, message: "User not found" }); 
+        }
+        returnData = response.data() || null;
+        return res.status(200).json({ status: 200, message: "success", data: returnData }); 
+    }
+
+    if (!team && !email && !uid) {
+        return res.status(400).json({ status: 400, message: "Missing user identifier (uid, email, or team)" });
+    }
+
+    let userCollection: FirebaseFirestore.CollectionReference<FirebaseFirestore.DocumentData>;
+    let query: FirebaseFirestore.Query<FirebaseFirestore.DocumentData> | undefined;
+
+    // set collection
+    userCollection = db.collection("users")
+    // add queries to collection based on params
+    if (team) query = userCollection.where("team", "==", team);
+    if (email) query = userCollection.where('email', '==', email);
+    response = await query?.get();
+
+    // return 404 if user doc not found
+    if (response?.empty) {
+        return res.status(404).json({ status: 404, message: "User not found" });
+    }
+
+    returnData = response && response.docs[0].data() ? response.docs[0].data() : null;
+    return res.status(200).json({ status: 200, message: "success", data: returnData });
+
+
 }
