29935ff54bfc88707c6e330d5f8c6223bedc43a4
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-07-08 19:47:36 +0100

    refactor firestore get

--- pages/api/teams/get.ts
+++ pages/api/teams/get.ts
@@ -1,23 +1,45 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { db } from "../../../firebaseAdmin"; 
+import { db } from "../../../firebaseAdmin";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
+    // decodeURI for team names with spaces
+    let teamID = req.query.team_id as string;
+    let teamName = req.query.team as string;
+    teamName = teamName ? decodeURI(teamName) : teamName;
 
-    const teamID = req.query.team_id as string;
+    // let response: FirebaseFirestore.DocumentSnapshot<FirebaseFirestore.DocumentData> | FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData> | null = null;
+    let teamData: FirebaseFirestore.DocumentData | null = null;
+    let docExists: boolean = false;
+    let docId: string | null = null;
 
-    if (!teamID) {
-        res.status(400).json({status: 400, message: "Missing team_id"});
+    if (!teamID && !teamName) {
+        return res.status(400).json({ status: 400, message: "Missing Team Identifier" });
     }
 
-    // get team by id
-    const response = await db.collection('teams').doc(teamID).get();
-    
-    if (!response.exists) {
-        res.status(404).json({status: 404, message: "Team not found"});
+    if (teamID) {
+        // get team by id
+        let response = await db.collection('teams').doc(teamID).get();
+        docExists = response.exists;
+        teamData = docExists ? response.data() as FirebaseFirestore.DocumentData | null : null;
+        docId = docExists ? response.id : null;
     }
 
-    res.status(200).json({status: 200, message: "success", data: response});
+    if (teamName) {
+        // get team by name
+        let response = await db.collection('teams').where('name', '==', teamName).limit(1).get();
+        docExists = !response.empty;
+        teamData = docExists ? response.docs[0].data() : null;
+        docId = docExists ? response.docs[0].id : null;
+    }
+
+    if (!teamData || !docExists) {
+        return res.status(404).json({ status: 404, message: "Team not found", data: { teamExists: docExists } });
+    }
+
+    const data = { teamId: docId, teamExists: docExists, ...teamData};
+
+    return res.status(200).json({ status: 200, message: "success", data });
 }
