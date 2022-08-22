16993db3bc0d58f899e5e793445d880466408f94
Ben Bromley <74667456+Ben-Bromley@users.noreply.github.com>
2022-08-22 23:28:56 +0100

    add teammates list to dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,18 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAuth } from '../context/AuthContext'
 import { useRouter } from 'next/router'
 
 const Login: NextPage = () => {
   // @ts-ignore
-  const { user, userData, loading, logout } = useAuth()
+  const { user, userData, loading, logout, teamMembers } = useAuth()
   const router = useRouter()
+  const [teamMemberHTML, setTeamMemberHTML] = useState(null);
 
   useEffect(() => {
     if (!loading && !user) {
       router.push('/login');
+    }
+
+    if (teamMembers) {
+      setTeamMemberHTML(teamMembers.map((member, index) => {
+        return <li key={index} className="bg-gray-500 p-4">{member.name}</li>
+      }))
     }
   }, [user, loading]);
 
@@ -23,7 +30,7 @@ const Login: NextPage = () => {
         <nav className='flex flex-col h-full sm:flex-row justify-between items-center p-8'>
           <h1>Dashboard for <strong>{userData?.email || "..."}</strong></h1>
           <div className='flex mt-4 sm:mt-0 space-x-4'>
-            <button className='p-2 border-2 font-bold border-blue-800 text-center px-6 rounded bg-blue-800 hover:bg-blue-900 hover:border-blue-900 transition-all'>New Game</button>
+            <a href="/games/new" className='p-2 border-2 font-bold border-blue-800 text-center px-6 rounded bg-blue-800 hover:bg-blue-900 hover:border-blue-900 transition-all'>New Game</a>
             <button className="p-2 border-2 font-bold px-6 rounded transition-all hover:text-gray-400 hover:border-gray-400" onClick={logout}>Logout</button>
           </div>
         </nav>
@@ -33,10 +40,7 @@ const Login: NextPage = () => {
           <section className='m-2 basis-72 bg-gray-700 rounded w-1/2 grow'>
             <h2 className='p-4'>Leaderboard</h2>
             <ul>
-              <li className='bg-gray-600 p-4'>#1 Ben D</li>
-              <li className='bg-gray-500 p-4'>#2 Rich</li>
-              <li className='bg-gray-600 p-4'>#3 Jacob</li>
-              <li className='bg-gray-500 p-4'>#4 Ryan</li>
+              {teamMemberHTML}
             </ul>
             <p className='text-center text-sm text-gray-400 p-2 my-2'>Add More Team Members</p>
           </section>
